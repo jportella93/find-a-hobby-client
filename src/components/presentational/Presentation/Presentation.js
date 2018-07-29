c9a4dc911f7d6a5391341256dcbedcaa24024c1d
jportella93 <jportella93@gmail.com>
2018-07-29 15:11:14 +0200

    feat: responsive presentation

--- src/components/presentational/Presentation/Presentation.js
+++ src/components/presentational/Presentation/Presentation.js
@@ -12,20 +12,20 @@ export default class Presentation extends Component {
           </div>
           <div className='color3 PresentationWrapper-colorBar'>
           </div>
-        </div>
-        <div className='Presentation'>
-          <h1>find a Hobby!</h1>
-          <br />
-          <h3>"Find three hobbies you love: <br /><br />
-            one to make you <span className='hobbyName'>money</span>, <br />
-            <i className="fas fa-dollar-sign"></i> <br />
-            <br />
-            one to keep you <span className='hobbyName'>in shape</span>, <br />
-            <i className="fas fa-football-ball"></i> <br />
-            <br />
-            and one to be <span className='hobbyName'>creative</span>."<br />
-            <i className="fab fa-fly"></i>
-          </h3>
+          <div className='Presentation'>
+            {/* <h1>find a Hobby!</h1>
+            <br /> */}
+            <h3>"Find three hobbies you love: <br /><br /><br />
+              one to make you <span className='hobbyName'>money</span>, <br />
+              <i className="fas fa-dollar-sign"></i> <br />
+              <br />
+              one to keep you <span className='hobbyName'>in shape</span>, <br />
+              <i className="fas fa-football-ball"></i> <br />
+              <br />
+              and one to be <span className='hobbyName'>creative</span>."<br />
+              <i className="fab fa-fly"></i>
+            </h3>
+          </div>
         </div>
       </div>
     )
